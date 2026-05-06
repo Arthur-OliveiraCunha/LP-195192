b8bebb9bed01048212dc8d0965cced80ed0c7aeb
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-05-06 09:42:31 -0300

    Create 1241.C

---
+++ 1241.C
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <string.h>
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Arthur de Oliveira Cunha>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1241
+Data        : 06/05/2026
+Objetivo    : Determinar se dois vetores tem numeros finais identicos
+Aprendizado : <<<Comparar vetores>>>
+-------------------------------------------------------------------------- */
+
+int main()
+{
+	int V[1000], W[1000];
+	int i=0;
+	
+	
+    for(int i = 0; i <= 100; i++){
+       
+       scanf("%d", V[ i ]);
+       scanf("%d", W[ i ]);
+}
+    i=(1000-i);
+    
+    if(V[i]=W[i]){
+        
+        printf("encaixa");
+    }    
+        
+    else{
+            
+        printf("nao encaixa");
+            
+    }
+    
+    
+	return 0;
+
+}
