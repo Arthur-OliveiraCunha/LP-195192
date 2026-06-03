41a7afd816b71a3005aa2630a85eb841e86ba7d2
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-06-03 10:51:23 -0300

    Create 1534.a.C

---
+++ 1534.a.C
@@ -0,0 +1,46 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Linguagem   : C
+Nome        : Arthur de Oliveira Cunha
+Plataforma  : https://judge.beecrowd.com/pt/problems/view/1534
+Exercício   : 1534.a
+Data        : 03/06/2026
+Objetivo    : <<<Imprimir matriz>>>
+Aprendizado : <<<Matrizes>>>
+-------------------------------------------------------------------------- */
+#include <stdio.h>
+ 
+int main() {
+    int A;
+    
+    while(scanf("%d", &A) != EOF){
+
+        int M[70][70];
+
+        for(int i=0; i < A; i++){
+
+            for(int j=0; j < A;j++){
+                
+                if(j == A - 1 - i){
+                    M[i][j] = 2;
+                }
+                else if(i == j){
+                    M[i][j] = 1;
+                }
+                else{
+                    M[i][j] = 3;
+                }
+            }
+        }
+        for(int i=0; i < A; i++){
+           
+                for(int j=0; j < A; j++){
+               
+                printf("%d", M[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    
+    return 0;
+}
