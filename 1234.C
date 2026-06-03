e7bb02928c5882f43153615325bc3c5438023153
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-06-03 10:23:08 -0300

    Create 1234.C

---
+++ 1234.C
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Arthur de Oliveira Cunha>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1234
+Data        : 03/06/2026
+Objetivo    : Sentenca dancante.
+Aprendizado : <<<Alterar caixa alta/baixa em uma sentenca>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+
+
+#include <string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+int main(){
+    int n, maiuscula;
+    char sentenca[51];
+
+    while(gets(sentenca) != NULL){
+        maiuscula = 1;
+        n = strlen(sentenca);
+
+        for(int i = 0; i < n; ++i){
+            if(!isalpha(sentenca[i]))  continue;
+
+            if(maiuscula)   sentenca[i] = toupper(sentenca[i]);
+            else            sentenca[i] = tolower(sentenca[i]);
+            maiuscula = 1 - maiuscula;
+        }
+
+        printf("%s\n", sentenca);
+    }
+
+    return 0;
+}
