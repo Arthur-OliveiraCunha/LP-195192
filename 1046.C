9e5ad51e9520d8b3f53489e6ff158fe1d6c98e90
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-06-23 21:48:23 -0300

    Update 1046.C

--- 1046.C
+++ 1046.C
@@ -5,32 +5,39 @@ Linguagem   : C
 Problema    : https://judge.beecrowd.com/pt/problems/view/1046
 Data        : 04/06/2026
 Objetivo    : Determinar a duracao de um jogo.
-Aprendizado : <<<Uso da funcao "calcularDuracao">>>
+Aprendizado : <<<Uso da funcao "funcao recursiva">>>
 Pergunta de segunda ordem: <<<Fazer a pergunta>>>
 -------------------------------------------------------------------------- */
 
 #include <stdio.h>
 
-int calcularDuracao(int INICIO, int FIM){
-    int DURACAO;
-    
-    if(INICIO == FIM){
-        DURACAO = 24;
-    }else{
-        DURACAO = ( (FIM - INICIO) + 24 ) % 24;
-    }
-    
-    return DURACAO;
+int Duracao(int S, int E) {
 
+    int D;
+
+    if (S == E) {
+        D = 24;
+        
+    } else {
+
+        D = ((E - S) + 24) % 24;
+    }
+
+    return D;
+    
 }
 
-int main() {
-    int INICIO, FIM;
-    
-    scanf("%d", &INICIO);
-    scanf("%d", &FIM);
-    
-    printf("O JOGO DUROU %d HORA(S)\n", calcularDuracao(INICIO, FIM));
- 
+int main () {
+
+    int S, E, D;
+
+    scanf("%d", &S);
+    scanf("%d", &E);
+
+    D = ((E - S) + 24);
+
+    printf("O JOGO DUROU %d HORA(S)\n", Duracao(S, E));
+
     return 0;
 }
+//codigo maldito, beecrowd seu odio por mim e reciproco.
