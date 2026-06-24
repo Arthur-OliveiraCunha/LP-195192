7626e903de33dc636e7b583480f6cda239c41259
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-06-24 17:15:48 -0300

    Create 1028.C

---
+++ 1028.C
@@ -0,0 +1,37 @@
+/* --------------------------------------------------------------------------
+Disciplina  : Lógica de Programação, turma IB, 2026S1
+Nome        : <<<Arthur de Oliveira Cunha>>>
+Linguagem   : C
+Problema    : https://judge.beecrowd.com/pt/problems/view/1028
+Data        : 24/06/2026
+Objetivo    : 
+Aprendizado : <<<Descrever o que foi aprendido com este exercício>>>
+Pergunta de segunda ordem: <<<Fazer a pergunta>>>
+-------------------------------------------------------------------------- */
+
+#include <stdio.h>
+
+int mdc(int a, int b) {
+    int resto;
+    while (b != 0) {
+        resto = a % b;
+        a = b;
+        b = resto;
+    }
+    return a;
+}
+
+int main() {
+    int N, F1, F2;
+
+    if (scanf("%d", &N) == 1) {
+        while (N--) {
+            
+            scanf("%d %d", &F1, &F2);
+  
+            printf("%d\n", mdc(F1, F2));
+        }
+    }
+
+    return 0;
+}
