9ff0819fa1dbf192cca23a23f2dd13b58586111b
Arthur-OliveiraCunha <arthur.cunha@unifesp.br>
2026-06-25 08:19:06 -0300

    Update Trabalho.C

--- Trabalho.C
+++ Trabalho.C
@@ -16,7 +16,6 @@ Pergunta de segunda ordem: <<<Fazer a pergunta>>>
 bool validar_linha_ou_coluna(int matriz[9][9], int index, int modo) {
     bool visto[10] = {false};
     for (int k = 0; k < 9; k++) {
-        
         int numero = (modo == 0) ? matriz[index][k] : matriz[k][index];
 
         if (numero == 0) continue;
@@ -28,7 +27,6 @@ bool validar_linha_ou_coluna(int matriz[9][9], int index, int modo) {
 }
 
 bool verificar_tabuleiro_completo(int matriz[9][9]) {
-    
     for (int i = 0; i < 9; i++) {
         if (!validar_linha_ou_coluna(matriz, i, 0) || !validar_linha_ou_coluna(matriz, i, 1)) {
             return false;
@@ -75,18 +73,29 @@ bool checar_vitoria(int matriz[9][9]) {
 }
 
 void exibir_alerta(char *mensagem) {
-    printf("%s\n======================================\n", mensagem);
+    printf("%s\n======================================\n", message);
 }
 
 int main() {
+    
     int matriz[9][9] = {
-        {5, 3, 0, 0, 7, 0, 0, 0, 0}, {6, 0, 0, 1, 9, 5, 0, 0, 0}, {0, 9, 8, 0, 0, 0, 0, 6, 0},
-        {8, 0, 0, 0, 6, 0, 0, 0, 3}, {4, 0, 0, 8, 0, 3, 0, 0, 1}, {7, 0, 0, 0, 2, 0, 0, 0, 6},
-        {0, 6, 0, 0, 0, 0, 2, 8, 0}, {0, 0, 0, 4, 1, 9, 0, 0, 5}, {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        {0, 3, 0, 5, 7, 0, 4, 6, 0},
+        {0, 9, 0, 2, 0, 1, 3, 7, 5},
+        {0, 5, 6, 0, 8, 4, 0, 1, 0},
+        {6, 0, 0, 0, 5, 0, 7, 9, 2},
+        {5, 0, 1, 0, 9, 3, 8, 0, 0},
+        {0, 0, 0, 4, 0, 0, 0, 3, 1},
+        {2, 0, 0, 9, 3, 5, 0, 8, 0},
+        {3, 6, 5, 8, 0, 7, 0, 2, 0},
+        {0, 7, 0, 6, 4, 2, 1, 5, 3}
     };
     
     int original[9][9];
-    for(int i=0; i<9; i++) for(int j=0; j<9; j++) original[i][j] = matriz[i][j];
+    for(int i = 0; i < 9; i++) {
+        for(int j = 0; j < 9; j++) {
+            original[i][j] = matriz[i][j];
+        }
+    }
 
     int linha, coluna, valor;
 
@@ -131,9 +140,8 @@ int main() {
         printf("======================================\n");
     }
 
-    
+
     return 0;
 
-    
+
 }
-// e assim se encerra minha curta carreira em programacao.. que venha Materiais kkk
